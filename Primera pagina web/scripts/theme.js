dbb790483be83d2f60d423c5c5b16b6e3b7baf34
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2022-04-01 19:02:17 -0300

    Dark mode, felxbox header
    Added a dark mode button and light and dark mode for the page
    Changed the header of the page to use flexbox

--- Primera pagina web/scripts/theme.js
+++ Primera pagina web/scripts/theme.js
@@ -4,14 +4,14 @@ dark = false;
 function switchTheme() {
     if (!dark) {
     document.body.className="white-theme";
-    document.getElementById("pito").style.backgroundColor = "#b9c0c9";
+    document.getElementById("hd").style.backgroundColor = "#b9c0c9";
     document.getElementById("dark-light-switch").style.backgroundImage = "url('icons/dark_mode_icon.svg')";
     document.getElementById("dark-light-switch").style.backgroundSize = "cover";
     dark=true;
     }
     else {
         document.body.className="dark-theme";
-        document.getElementById("pito").style.backgroundColor = "#385170";
+        document.getElementById("hd").style.backgroundColor = "#385170";
         document.getElementById("dark-light-switch").style.backgroundImage = "url('icons/light_mode_icon.svg')";
         document.getElementById("dark-light-switch").style.backgroundSize = "cover";
         dark = false;
